7a3e5cffe71007e04557138161dbd8095c0cb236
ketdougi <70666840+ketdougi@users.noreply.github.com>
2022-02-24 09:41:33 -0800

    Add import to BrowserAcceptanceTest.java
    Add ChromeOptions import

--- src/test/java/com/giorgiosironi/gameoflife/BrowserAcceptanceTest.java
+++ src/test/java/com/giorgiosironi/gameoflife/BrowserAcceptanceTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
